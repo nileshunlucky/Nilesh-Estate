ec5ed4f10723df4248f402cb505d294770bb9df7
unluckynilesh <nileshinde001@gmail.com>
2024-10-12 10:32:29 +0530

    added search functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const navigate = useNavigate()
+
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <nav className='bg-zinc-300 text-black h-[70px] flex justify-between items-center md:px-[100px] px-[10px] gap-3 shadow-md shadow-zinc-400 select-none'>
       <div className="logo">
@@ -12,9 +32,9 @@ const Navbar = () => {
         </NavLink>
       </div>
       <div className="search">
-        <form className='bg-zinc-100 p-2 rounded flex justify-between items-center'>
-          <input className='lg:w-[500px] w-[100px] focus:outline-none bg-transparent' type="text" placeholder="Search" />
-          <button type='submit'>
+        <form onSubmit={hundleSubmit} className='bg-zinc-100 p-2 rounded flex justify-between items-center'>
+          <input onChange={e => setSearchTerm(e.target.value)} className='lg:w-[500px] w-[100px] focus:outline-none bg-transparent' type="text" placeholder="Search" />
+          <button>
             <i className="fa-solid fa-magnifying-glass text-zinc-600 cursor-pointer px-3 text-[17px]"></i>
           </button>
         </form>
--- server/routes/listing.routes.js
+++ server/routes/listing.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
-import { createListing, deleteListing , getListing} from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, getListings } from "../controllers/listing.controller.js";
 const router = express.Router();
 
-router.post("/create", verifyToken ,createListing);
-router.delete("/delete/:id", verifyToken ,deleteListing);
-router.post("/update/:id", verifyToken ,createListing);
-router.get("/get/:id" ,getListing);
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, createListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router
